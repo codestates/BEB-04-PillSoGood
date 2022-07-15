64e181f8587c0bff004b68d3c07c8358496e8f15
genius-jihye-park <sconenomad@gmail.com>
2022-07-15 09:41:19 +0900

    Tabs, Cards, PrimaryButton, Home, Charactor, Health, Reminders

---
+++ pillsogood-client/screens/Charactor.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from "react-native"
+
+
+const Charactor = () => (
+  <View>
+    <Text>Charactor</Text>
+  </View>
+);
+export default Charactor;
+
+

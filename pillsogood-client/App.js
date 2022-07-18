f056ed4fdafa7188b7f455adee43eddf2358b2cd
minwoogramer <01082249606nmw@gmail.com>
2022-07-18 16:04:59 +0900

    Docs:Firebase push

--- pillsogood-client/App.js
+++ pillsogood-client/App.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { ApolloProvider } from "@apollo/client";
-import { Alert } from "react-native";
-import messaging from "@react-native-firebase/messaging";
+
 import client from "./apolloClient";
-import auth from "@react-native-firebase/auth";
-import OutNav from "./navigators/OutNav";
-import InNav from "./navigators/InNav";
+
 import { Provider } from "react-redux";
 import { store } from "./src/store";
 import {
@@ -15,23 +12,23 @@ import {
   GetFCMToken,
 } from "./src/utils/Pushnotification";
 
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+useEffect(() => {
+  auth().onAuthStateChanged((user) => {
+    //인증상태감지
+    if (user) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  });
+  console.log(auth().currentUser);
+}, []);
 export default function App() {
   useEffect(() => {
     GetFCMToken();
     requestUserPermission();
     NotificationListener();
-  }, []);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(() => {
-    auth().onAuthStateChanged((user) => {
-      //인증상태감지
-      if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    });
-    console.log(auth().currentUser);
   }, []);
 
   return (

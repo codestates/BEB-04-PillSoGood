d697dbf968970896af874513874e8c49e76b1e54
jihye_Park <sconenomad@gmail.com>
2022-07-20 14:37:44 +0900

    Feat: Home화면 구현 #1

--- pillsogood-client/screens/Home.js
+++ pillsogood-client/screens/Home.js
@@ -1,64 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components/native";
 import { BASE_COLOR } from "../colors";
+import { StatusBar, Text, View, FlatList } from "react-native";
 
 
-const DATA = ['고지혈증', '고혈압', '당뇨'];
-
-// const Container1 = styled.Container`
-//   background-color: ${BASE_COLOR};
-//   flex: 1;
-//   marginTop: StatusBar.currentHeight || 0,
-// `;
-const View1 = styled.View`
-  width: 100%;
-  margin-top: 10px;
-  padding: 10px 20px;
-  border-radius: 20px;
-  margin-bottom: 10px;
-  font-size: 16px;
+const Container = styled.View`
+  background-color: "beige",
+  flex: 1;
+  justify-content: "space-between",
   color: black;
-  background-color: '#DDECCA';
+  padding: 20px,
+  margin: 10px,
 `;
-const FlatList1 = styled.FlatList`
+const Card = styled.View`
+  flex: 0.3,
+  background-color: "beige",
+  border-radius: 20px,
+`;
+
+const LoginBtn = styled.TouchableOpacity`
+  margin-top: 50px;
+  width: 60%;
+  padding: 20px 20px;
+  border-width: 1px;
+  border-radius: 30px;
+  border-color: rgba(255, 255, 255, 0.5);
+  justify-content:"space-between"
+  align-items: center;
+
+  background-color: #202d35;
+`;
+const BtnText = styled.Text`
+  color: white;
   font-size: 16px;
-  text-align: center;
-  color: black;
-  border-radius: 20px;
 `;
-const Text1 = styled.Text`
-  textAlign: 'center'
-  backgroundColor: '#DDECCA',
-  padding: 20,
-  fontSize: 20,
-  `;
 
-const Home = () => (
-    <View1>
-      <Text1> 메인
-      </Text1>
-      <FlatList1
-        data={DATA}
-        renderItem={({ item }) =>
-          <Text1>
-            {item}
-          </Text1>}
-      />
-      <View1
-        style={{
-          height: 25,
-          backgroundColor: Colors.pink,
-          borderWidth: 0,
-          width: 25,
-          marginLeft: -26,
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: 50,
-        }}>
-        <Text1> 
-        </Text1>
-      </View1>
-    </View1>
 
-);
+const Home = () => {
+const DATA = ["고지혈증", "고혈압", "당뇨"];
+const [visible, setVisible] = useState(true);
+const onPress = () => {
+  setVisible(!visible);
+  }
+  return <Container>
+    <Text> 메인</Text>
+    <Card data={DATA} renderItem={({ item }) => <Text>{item}</Text>} />
+    <LoginBtn>
+      <BtnText onPress={onPress}> 약 복용 하셨나요?
+      </BtnText>
+    </LoginBtn>
+  </Container>
+};
 export default Home;
+

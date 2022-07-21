534e09fb4efe8bf11f151c61d5d344ac3620ff2e
haejeonghy <haejeonghy@gmail.com>
2022-07-21 17:56:18 +0900

    Feat: 목록 표 css 적용 - styled components

---
+++ pillsogood-admin/components/StyledTable.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledTable = styled.table`
+    width: 80%;
+    padding: 10px 0;
+    border-collapse: collapse;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+export const StyledTr = styled.tr`
+`
+
+export const StyledTh = styled.th`
+    text-align: center;
+    font-weight: 500;
+    color: #ffffff;
+    padding: 5px 0;
+    background-color: #5c5c5c;
+`
+
+export const StyledTd = styled.td`
+    text-align: center;
+    padding: 5px 0;
+    border-bottom: 1px solid #B5B5B5;
+`

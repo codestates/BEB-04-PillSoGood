b083f073cf14118d5455502b0af851e97f5ad649
jihye_Park <sconenomad@gmail.com>
2022-07-21 12:21:59 +0900

    Feat: Splash_Screen 구현완료

--- pillsogood-client/App.js
+++ pillsogood-client/App.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { ApolloProvider } from "@apollo/client";
-
+import SplashScreen from 'react-native-splash-screen';
 import client from "./apolloClient";
 import auth from "@react-native-firebase/auth";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
+import Root from "./navigators/Root";
 import {
   requestUserPermission,
   NotificationListener,
   GetFCMToken,
 } from "./src/utils/Pushnotification";
-import Root from "./navigators/Root";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -30,12 +30,14 @@ export default function App() {
     GetFCMToken();
     requestUserPermission();
     NotificationListener();
+    SplashScreen.hide();
   }, []);
+
   return (
     <ApolloProvider client={client}>
       <Provider store={store}>
         <NavigationContainer>
-          <Root />
+        <Root />
         </NavigationContainer>
       </Provider>
     </ApolloProvider>

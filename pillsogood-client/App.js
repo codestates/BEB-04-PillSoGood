f406bac02a9b6f9bf4baf717f49991b39f3928ca
minwoogramer <01082249606nmw@gmail.com>
2022-07-21 15:41:38 +0900

    Docs:에러 주석처리

--- pillsogood-client/App.js
+++ pillsogood-client/App.js
@@ -18,11 +18,11 @@ import { getPermission, getPermissions } from "./src/utils/Permissons";
 import { request } from "react-native-permissions";
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [openSettingsForNotifications] = useMMKVStorage(
-    "openSettingsForNotifications",
-    MMKV,
-    false
-  );
+  // const [openSettingsForNotifications] = useMMKVStorage(
+  //   "openSettingsForNotifications",
+  //   MMKV,
+  //   false
+  // );
   useEffect(() => {
     auth().onAuthStateChanged((user) => {
       //인증상태감지
@@ -35,33 +35,33 @@ export default function App() {
     console.log(auth().currentUser);
   }, []);
   //ForeGround state에서 message 받기
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-    });
+  // useEffect(() => {
+  //   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+  //     Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+  //   });
 
-    return unsubscribe;
-  }, []);
+  //   return unsubscribe;
+  // }, []);
   //background state에서 알람 처리
-  useEffect(() => {
-    if (openSettingsForNotifications) {
-      navigate("NotificationsSettingsScreen");
-    }
-  }, [openSettingsForNotifications]);
+  // useEffect(() => {
+  //   if (openSettingsForNotifications) {
+  //     navigate("NotificationsSettingsScreen");
+  //   }
+  // }, [openSettingsForNotifications]);
   //quit state에서 처리
-  useEffect(() => {
-    messaging()
-      .getDidOpenSettingsForNotification()
-      .then(async (didOpenSettingsForNotification) => {
-        if (didOpenSettingsForNotification) {
-          navigate("NotificationsSettingsScreen");
-        }
-      });
-  }, []);
+  // useEffect(() => {
+  //   messaging()
+  //     .getDidOpenSettingsForNotification()
+  //     .then(async (didOpenSettingsForNotification) => {
+  //       if (didOpenSettingsForNotification) {
+  //         navigate("NotificationsSettingsScreen");
+  //       }
+  //     });
+  // }, []);
   useEffect(() => {
     GetFCMToken();
     requestUserPermission();
-    NotificationListener();
+    // NotificationListener();
     SplashScreen.hide();
   }, []);
   return (

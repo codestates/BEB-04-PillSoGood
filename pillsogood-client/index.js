3eb13a61065bd385c3d912c36d6377861ee9b6b9
haejeonghy <haejeonghy@gmail.com>
2022-07-27 17:36:59 +0900

    Fix: 클라이언트 제외 커밋

--- pillsogood-client/index.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from "expo";
-import App from "./App";
-import messaging from "@react-native-firebase/messaging";
-
-messaging().setOpenSettingsForNotificationsHandler(async () => {
-  //MMKV 패키지가 무엇일까..
-  MMKV.setBool(openSettingsForNotifications, true);
-});
-registerRootComponent(App);
